bb077c8b36d920c982a0e1bc58259fa79b852422
Dragomir Angelov <dragomir.angelov@voicecom.bg>
2018-03-20 12:17:24 +0200

    Bug fix

--- html/common/header.php
+++ html/common/header.php
@@ -22,50 +22,50 @@
         <div class="container">
 
             <div class="navbar-header">
-                <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+                <a class="navbar-brand" href="<?= BASE_URL; ?>index.php">CodeSnip</a>
+
+                <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbarMain">
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-
-                <a class="navbar-brand" href="<?= BASE_URL; ?>index.php">CodeSnip</a>
             </div>
 
-            <div id="navbar-main" class="collapse navbar-collapse">
-            <?php if (is_logged()) : ?>
+            <div class="collapse navbar-collapse" id="navbarMain">
+                <?php if (is_logged()) : ?>
 
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="<?= BASE_URL; ?>index.php">Home</a>
-                    </li>
-                    <?php if (is_admin()) : ?>
+                    <ul class="nav navbar-nav">
                         <li>
-                            <a href="<?= BASE_URL; ?>account_approve.php">Approve Accounts</a>
+                            <a href="<?= BASE_URL; ?>index.php">Home</a>
                         </li>
-                    <?php endif; ?>
+                        <?php if (is_admin()) : ?>
+                            <li>
+                                <a href="<?= BASE_URL; ?>account_approve.php">Approve Accounts</a>
+                            </li>
+                        <?php endif; ?>
 
-                </ul>
+                    </ul>
 
-                <div class="navbar-header">
-                    <span class="navbar-text">Welcome, <?= $_SESSION["firstname"] . " " . $_SESSION["lastname"]; ?></span>
-                </div>
-
-                <form class="navbar-form navbar-right" action="<?= BASE_URL; ?>include/entry/logout.php" method="POST">
-                    <button class="btn btn-default" type="submit" name="submit">Logout</button>
-                </form>
-            <?php else : ?>
-
-                <form id="login" class="collapse navbar-collapse navbar-form navbar-right" action="<?= BASE_URL; ?>include/entry/login.php" method="POST">
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="usr" placeholder="Username">
+                    <div class="navbar-header">
+                        <span class="navbar-text">Welcome, <?= $_SESSION["firstname"] . " " . $_SESSION["lastname"]; ?></span>
                     </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" name="pwd" placeholder="Password">
-                    </div>
-                    <button name="submit" type="submit" class="btn btn-default">Sign in</button>
-                    <a class="btn-signup btn btn-default" href="<?= BASE_URL; ?>signup.php">Sign up</a>
-                </form>
-            <?php endif; ?>
+
+                    <form class="navbar-form navbar-right" action="<?= BASE_URL; ?>include/entry/logout.php" method="POST">
+                        <button class="btn btn-default" type="submit" name="submit">Logout</button>
+                    </form>
+                <?php else : ?>
+
+                    <form id="login" class="navbar-form navbar-right" action="<?= BASE_URL; ?>include/entry/login.php" method="POST">
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="usr" placeholder="Username">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" name="pwd" placeholder="Password">
+                        </div>
+                        <button name="submit" type="submit" class="btn btn-default">Sign in</button>
+                        <a class="btn-signup btn btn-default" href="<?= BASE_URL; ?>signup.php">Sign up</a>
+                    </form>
+                <?php endif; ?>
 
             </div>
         </div>
