53eaef5b90e3fa80523aefdc91fddeda0cd36a81
Dragomir Angelov <dragomir.angelov@voicecom.bg>
2018-04-05 13:20:29 +0300

    Change to settings file and main constants

--- include/settings.php
+++ include/settings.php
@@ -1,23 +1,24 @@
 <?php
 
-// Define main constants
-if ($_SERVER['SERVER_ADDR'] == '::1') { // Localhost
-    define('ROOT', "C:/xampp/htdocs/CodeSnip/");
-    define('BASE_URL', "http://localhost/CodeSnip/");
-    define("DB_HOST", "localhost");
-    define("DB_NAME", "code_snip");
-    define("DB_USER", "root");
-    define("DB_PASS", "");
-}
+// Define BASE URL and ROOT
+define('ROOT', dirname(__DIR__) . '/');
+define('BASE_URL', "http://" . $_SERVER['HTTP_HOST'] . pathinfo($_SERVER['PHP_SELF'])['dirname'] . '/');
+
+// Define database settings
+$settings = parse_ini_file(ROOT . 'include/config/config.ini', true);
+define("DB_HOST", $settings['db']['host']);
+define("DB_NAME", $settings['db']['name']);
+define("DB_USER", $settings['db']['user']);
+define("DB_PASS", $settings['db']['pass']);
+
+// Include constants
+require_once ROOT . 'include/config/constants.php';
 
 // Start db connection
 require_once ROOT . 'include/config/db_connect.php';
 
 // Start sessions
 require_once ROOT . 'include/config/session_start.php';
-
-// Include constants
-require_once ROOT . 'include/config/constants.php';
 
 // Include functions
 require_once ROOT . 'include/config/functions.php';
