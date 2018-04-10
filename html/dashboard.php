0ff36a2d3d24ab91380a5635618253f0426a77df
Dragomir Angelov <dragomir.angelov@voicecom.bg>
2018-04-10 17:44:03 +0300

    Bug fixes and style fix

--- html/dashboard.php
+++ html/dashboard.php
@@ -2,6 +2,7 @@
 
 <h2>Моите парчета код</h2>
 <div class="panel-group" id="accordion">
+
     <?php
     $query = 'SELECT id, title, text, lang, created_on, updated_on
             FROM snippet
@@ -13,33 +14,31 @@
     $snippets = $stmt->fetchAll(PDO::FETCH_ASSOC);
     ?>
 
-    <?php foreach ($snippets
+    <?php foreach ($snippets as $snip) : ?>
+        <div class="panel panel-default">
 
-    as $snip) : ?>
-    <div class="panel panel-default">
-        <!-- Add to div data-parent="#accordion" -->
-        <div class="panel-heading">
-            <div class="panel-title">
-
-                <div style="float: right">
-                    <h4 style="display: inline"><span class="label label-info">Дата на създаване: <?= date('d.m.Y', strtotime($snip['created_on'])); ?></span></h4>
-                    <h4 style="display: inline"><span  class="label label-success">Последно редактиране: <?= date('d.m.Y', strtotime($snip['updated_on'])); ?></span></h4>
+            <div class="panel-body">
+                <div class="panel-title">
+                    <h4><strong><?= $snip['title']; ?></strong></h4>
                 </div>
 
                 <div class="row">
-                    <div class="col-md-3">
-                        <h4><strong><?= $snip['title']; ?></strong></h4>
-                    </div>
-                    <div class="col-md-7"></div>
 
-                    <div class="col-md-2">
-                        <button class="btn btn-snip btn-float-right">
+                    <div class="col-md-11">
+                        <h4 style="display: inline"><span class="label label-info">Дата на създаване: <?= date('d.m.Y', strtotime($snip['created_on'])); ?></span></h4>
+                        <h4 style="display: inline"><span class="label label-success">Последно редактиране: <?= date('d.m.Y', strtotime($snip['updated_on'])); ?></span></h4>
+                    </div>
+
+                    <div class="col-md-1">
+
+                        <button class="btn btn-snip">
                             <span class="glyphicon glyphicon-option-horizontal"></span>
                         </button>
 
-                        <button class="btn btn-snip btn-float-right">
+                        <button class="btn btn-snip">
                             <span class="glyphicon glyphicon-share-alt"></span>
                         </button>
+
                     </div>
                 </div>
 
@@ -47,14 +46,13 @@
                     <span class="glyphicon glyphicon-chevron-down"></span>
                 </button>
             </div>
-        </div>
 
-    <div id="snip_<?= $snip['id']; ?>" class="panel-collapse collapse">
-        <div class="panel-body">
-            <pre><code class="<?= $snip['lang']; ?> hljs"><?= $snip['text']; ?></code></pre>
+            <div id="snip_<?= $snip['id']; ?>" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <pre><code class="<?= $snip['lang']; ?> hljs"><?= $snip['text']; ?></code></pre>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-<?php endforeach; ?>
+    <?php endforeach; ?>
 
 </div>
